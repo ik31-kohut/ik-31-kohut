9035621e7719031f2dee5570f07bd7069d71ca40
Taras Kohut <tar4ukkogut@gmail.com>
2019-11-26 15:04:25 +0200

    Lab_3: Add  working site

--- lab_3/main/views.py
+++ lab_3/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import datetime
+import platform
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+def health(request):
+    response = {'date': datetime.now(), 'current_page': request.path, 'server_info': {'system': platform.system(), 'node': platform.node(), 'release': platform.release(), 'version': platform.version(), 'machine': platform.machine(), 'processor': platform.processor()}, 'client_info': request.headers.get('User-Agent')}
+    return JsonResponse(response)
