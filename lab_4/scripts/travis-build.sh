505a4415aa7e0eef92e4717a45af89ceb080cdaf
Taras Kohut <tar4ukkogut@gmail.com>
2019-12-07 19:44:46 +0200

    Lab_6: Update script in lab_4

--- lab_4/scripts/travis-build.sh
+++ lab_4/scripts/travis-build.sh
@@ -3,7 +3,7 @@ set -ev
 docker build -t taras0101/lab4-examples:django-travis .
 docker images
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; docker push taras0101/lab4-examples:django-travis; else echo "PR skip deploy"; fi
-docker build -t taras0101/lab4-examples:django-travis .
+docker build -t taras0101/lab4-examples:monitoring-travis .
 docker images
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; docker push taras0101/lab4-examples:django-travis; else echo "PR skip deploy"; fi
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; docker push taras0101/lab4-examples:monitoring-travis; else echo "PR skip deploy"; fi
 exit 0
